86902ebcebb577efebd2fe7e3dc8879ea6ec1018
Akter Hussain <akterdev@gmail.com>
2022-07-12 22:49:03 +0600

    Requesr and Response

--- source/main.js
+++ source/main.js
@@ -1,6 +1,18 @@
 var http = require('http');
 var server = http.createServer(function(req, res){
-    res.end("Hello World");
+    if(req.url=="/"){
+        res.writeHead(200, {'Content-type':'text/html'})
+        res.write('<h1>This is Home Page</h1>')
+        res.end();
+    } else if (req.url=="/about"){
+        res.writeHead(200, {'Content-type':'text/html'})
+        res.write('<h1>This is about Page</h1>')
+        res.end();
+    } else if (req.url=="/contact"){
+        res.writeHead(200, {'Content-type':'text/html'})
+        res.write('<h1>This is contact Page</h1>')
+        res.end();
+    }
 });
 
 server.listen(5050);
