6f9e2f6b20dbadee46b5a5b02977a7f19e731a9e
Akter Hussain <akterdev@gmail.com>
2022-07-13 07:43:01 +0600

    URL module

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Node.js-Basic
+
+Note:
+1. using terminal for start server "node main.js"
+2. package.json files install "npm init --y"
+3. 
--- source/main.js
+++ source/main.js
@@ -1,18 +1,19 @@
 var http = require('http');
+var URL = require('url');
 var server = http.createServer(function(req, res){
-    if(req.url=="/"){
-        res.writeHead(200, {'Content-type':'text/html'})
-        res.write('<h1>This is Home Page</h1>')
-        res.end();
-    } else if (req.url=="/about"){
-        res.writeHead(200, {'Content-type':'text/html'})
-        res.write('<h1>This is about Page</h1>')
-        res.end();
-    } else if (req.url=="/contact"){
-        res.writeHead(200, {'Content-type':'text/html'})
-        res.write('<h1>This is contact Page</h1>')
-        res.end();
-    }
+    var myUrl = "https://www.facebook.com/search/top?q=jaima%20nur";
+    var urlObj = URL.parse(myUrl, true);
+    var myHostname = urlObj.host;
+    var myPathname = urlObj.pathname;
+    var mySearchname = urlObj.search;
+
+    res.writeHead(200, {'Content-type':'text/html'})
+    res.write(
+        "This is host name: "+ myHostname + "</br>" +
+        "This is Path name: "+ myPathname + "</br>" +
+        "This is Search name: "+ mySearchname
+    );
+    res.end();
 });
 
 server.listen(5050);
