dabeb1d48a1978d9df269c4dbb0ae8a4a43652c1
Akter Hussain <akterdev@gmail.com>
2022-07-14 06:51:43 +0600

    Read file symchronous method

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,11 +4,10 @@ var http = require('http');
 var server = http.createServer(function(req, res){
     if(req.url="/"){
         //asy
-        fs.readFile('home.html', function(error, data){
-            res.writeHead(200, {'Content-type':'text/html'});
-            res.write(data);
-            res.end();
-        });
+        let myData = fs.readFileSync('home.html');
+        res.writeHead(200, {'Content-type':'text/html'});
+        res.write(myData);
+        res.end();
     }
 });
 
