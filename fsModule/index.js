da8da2ea89af9cbb91580f401b8a378bf80eff33
Akter Hussain <akterdev@gmail.com>
2022-07-15 11:38:55 +0600

    delete file

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,17 +4,19 @@ var http = require('http');
 var server = http.createServer(function(req, res){
     if(req.url=="/"){
 
-        let error= fs.unlinkSync("demoUpdate.txt")
-        if(error){
-            res.writeHead(200, {'Content-type':'text/html'});
-            res.write("File Delete Fail");
-            res.end();
-        }
-        else{
-            res.writeHead(200, {'Content-type':'text/html'});
-            res.write("File Delete success with Sync method");
-            res.end();
-        }
+        fs.unlink("test.txt", function(error){
+            if(error){
+                res.writeHead(200, {'Content-type':'text/html'});
+                res.write("File Delete Fail");
+                res.end();
+            }
+            else{
+                res.writeHead(200, {'Content-type':'text/html'});
+                res.write("File Delete success");
+                res.end();
+            }
+        })
+
     }     
 });
 
