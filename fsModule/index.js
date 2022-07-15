0499f09557db9a86048a220fd39b8701804a148c
Akter Hussain <akterdev@gmail.com>
2022-07-15 12:07:58 +0600

    file exist

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,18 +4,13 @@ var http = require('http');
 var server = http.createServer(function(req, res){
     if(req.url=="/"){
 
-        fs.unlink("test.txt", function(error){
-            if(error){
-                res.writeHead(200, {'Content-type':'text/html'});
-                res.write("File Delete Fail");
-                res.end();
-            }
-            else{
-                res.writeHead(200, {'Content-type':'text/html'});
-                res.write("File Delete success");
-                res.end();
-            }
-        })
+        let result = fs.existsSync("name.txt");
+        if(result){
+            res.end('Worked successfully');
+        }
+        else{
+            res.end('File not found');
+        }
 
     }     
 });
