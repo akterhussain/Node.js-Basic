8a2c7face706357c5cbcf7fab4c51e16a9fb4838
Akter Hussain <akterdev@gmail.com>
2022-07-15 11:10:49 +0600

    File Write Success with syn method

--- fsModule/index.js
+++ fsModule/index.js
@@ -3,18 +3,17 @@ var http = require('http');
 
 var server = http.createServer(function(req, res){
     if(req.url=="/"){
-        fs.writeFile("demo.txt", "Hello Bangladesh", function(error){
-            if(error){
-                res.writeHead(200, {'Content-type':'text/html'});
-                res.write("File Write Fail");
-                res.end();
-            }
-            else{
-                res.writeHead(200, {'Content-type':'text/html'});
-                res.write("File Write Success");
-                res.end();
-            }
-        })
+        let error= fs.writeFileSync("demo.txt", "Hello Bangladesh")
+        if(error){
+            res.writeHead(200, {'Content-type':'text/html'});
+            res.write("File Write Fail");
+            res.end();
+        }
+        else{
+            res.writeHead(200, {'Content-type':'text/html'});
+            res.write("File Write Success with syn method");
+            res.end();
+        }
     }     
 });
 
