58594925032ff027ac333e372ccee5be4ec579f1
Akter Hussain <akterdev@gmail.com>
2022-07-16 09:09:09 +0600

    connection database

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -1,1 +1,10 @@
-index.js
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://akterhussain:YJtHJeTei5qfQP0T@cluster0.nud2y.mongodb.net/?retryWrites=true&w=majority";
+
+MongoClient.connect(url, function(error){
+    if(error){
+        console.log('Connection falid');
+    } else{
+        console.log('connection successfull');
+    }
+});
